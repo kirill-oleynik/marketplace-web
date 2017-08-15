24b3d1546ce89a61a8f5475aa098aafb1b366581
Valeriia Ibadullaieva <valeriia.ibadullaieva@gmail.com>
2017-08-15 21:07:03 +0300

    Add log in page

---
+++ app/components/all_rights.js
@@ -0,0 +1,8 @@
+export default () => (
+  <p className="font-12 in-black-035">
+    ©2017 All Rights Reserved. Appreview®.
+    <a href="/" className="rights-link">Privacy Policy</a>
+     and
+    <a href="/" className="rights-link">Terms of Use</a>
+  </p>
+);
--- app/components/main_input.js
+++ app/components/main_input.js
@@ -1,26 +1,42 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const MainInput = ({ name, label, hint, type, className }) => (
-  <div className={classNames('main-input', className)}>
-    <div className="main-input__field-box">
-      <input type={type} name={name} className="main-input__field" />
-      <label htmlFor={name} className="main-input__label">{label}</label>
+const MainInput = ({ name, label, hint, type, icon, className }) => {
+  if (icon) {
+    return (
+      <div className={classNames('main-input', className)}>
+        <div className="main-input__field-box">
+          <i className={classNames(`main-input__icon icon icon-${icon}`)} />
+          <input type={type} name={name} className="main-input__field" />
+          <label htmlFor={name} className="main-input__label">{label}</label>
+        </div>
+        <div className="main-input__hint">{hint}</div>
+      </div>
+    );
+  }
+  return (
+    <div className={classNames('main-input', className)}>
+      <div className="main-input__field-box">
+        <input type={type} name={name} className="main-input__field" />
+        <label htmlFor={name} className="main-input__label">{label}</label>
+      </div>
+      <div className="main-input__hint">{hint}</div>
     </div>
-    <div className="main-input__hint">{hint}</div>
-  </div>
-);
+  );
+};
 
 MainInput.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string,
   hint: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  icon: PropTypes.string,
   className: PropTypes.string
 };
 
 MainInput.defaultProps = {
   type: 'text',
+  icon: '',
   className: ''
 };
 
