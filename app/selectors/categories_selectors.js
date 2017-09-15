cf6e18ed3f7fe6cce41ccf1d72e15fe4c4e65a9d
kirill-oleynik <kirill.olejnik@gmail.com>
2017-09-15 18:51:23 +0300

    Show related applications

--- app/selectors/categories_selectors.js
+++ app/selectors/categories_selectors.js
@@ -4,9 +4,27 @@ import { getById as getApplicationsById } from './applications_selectors';
 export const getIds = (state) => state.categories.ids;
 export const getById = (state) => state.categories.byId;
 export const getCurrent = (state) => state.categories.current;
+export const getRelatedIds = (state) => {
+  const { appProfile: { categoriesIds } } = state.applications;
+  return categoriesIds || [];
+};
 
 export const getCategories = createSelector(
   getIds, getById, (ids, byId) => ids.map((id) => byId[id])
+);
+
+export const getRelatedCategories = createSelector(
+  getRelatedIds, getById, (ids, byId) => ids.map((id) => byId[id])
+);
+
+export const getRelatedCategoriesWithApplications = createSelector(
+  getRelatedCategories, getApplicationsById,
+  (categories, applications) => categories.map((category) => ({
+    ...category,
+    applications: category.applications.map((applicationId) => (
+      applications[applicationId]
+    ))
+  }))
 );
 
 export const getCategoriesWithApplications = createSelector(
